37cd478187d60b80a9b213d3a977abc840f237e4
Tomasz Kociołek <tomasz.kociolek@k2.pl>
2017-01-17 14:01:28 +0100

    missing object assign transformation

--- es5/index.js
+++ es5/index.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _simpleEventer = require('simple-eventer');
@@ -60,7 +62,7 @@ var Scaler = function (_SimpleEventer) {
 
     Scaler.singletonInstance = _this;
 
-    _this.settings = Object.assign({}, defaults, options);
+    _this.settings = _extends({}, defaults, options);
 
     _this.viewport = null;
     _this.segment = null;
